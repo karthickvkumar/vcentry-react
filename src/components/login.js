226cadac600a1f92f5ce08c49213ec66c6f6b36d
kvelraj_concord <kvelraj@concord.net>
2021-03-12 09:48:09 +0530

    checkbox option

--- src/components/login.js
+++ src/components/login.js
@@ -7,7 +7,10 @@ class LoginComponent extends Component {
     this.state = {
       username : '',
       password: '',
-      gender: ''
+      gender: '',
+      dob: '',
+      skills: [],
+      city: ''
     };
   }
 
@@ -17,9 +20,23 @@ class LoginComponent extends Component {
 
   onHandleInput(event){
     console.log(event.target.name , '--->', event.target.value)
-    this.setState({
-      [event.target.name] : event.target.value
-    })
+    console.log(event.target.checked)
+    if(event.target.name === 'skills'){
+      if(event.target.checked){
+        this.state.skills.push(event.target.value);
+      }else{
+        const index = this.state.skills.findIndex((value) => value === event.target.value);
+        this.state.skills.splice(index, 1);
+      }
+      this.setState({
+        skills : this.state.skills
+      })
+    }
+    else if(event.target.name !== 'skills'){
+      this.setState({
+        [event.target.name] : event.target.value
+      })
+    }
   }
   
 
@@ -33,11 +50,33 @@ class LoginComponent extends Component {
         <label>Enter Password:</label>
         <input type="password" placeholder="Enter password" name="password" onChange={this.onHandleInput.bind(this)}/>
         <br></br>
+        <label>Enter DOB:</label>
+        <input type="date" placeholder="Enter date of birth" name="dob" onChange={this.onHandleInput.bind(this)}/>
+        <br></br>
         <div>
           <label>Select your Gender:</label>
           <input type="radio" name="gender" value="male" onChange={this.onHandleInput.bind(this)}/>Male
           <input type="radio" name="gender" value="female" onChange={this.onHandleInput.bind(this)}/>Female
           <input type="radio" name="gender" value="na" onChange={this.onHandleInput.bind(this)} />Others
+        </div>
+        <div>
+          <label>Select your Skills:</label>
+          <input type="checkbox" name="skills" value="java" onChange={this.onHandleInput.bind(this)}/>JAVA
+          <input type="checkbox" name="skills" value="php" onChange={this.onHandleInput.bind(this)}/>PHP
+          <input type="checkbox" name="skills" value="python" onChange={this.onHandleInput.bind(this)}/>Python
+          <input type="checkbox" name="skills" value=".net" onChange={this.onHandleInput.bind(this)}/>.NET
+          <input type="checkbox" name="skills" value="react-js" onChange={this.onHandleInput.bind(this)}/>React JS
+        </div>
+
+        <div>
+          <label>Select your City:</label>
+          <select defaultValue="default" name="city" onChange={this.onHandleInput.bind(this)}>
+            <option disabled value="default">Please select any city</option>
+            <option value="CH">Chennai</option>
+            <option value="MDU">Madurai</option>
+            <option value="ERD">Erode</option>
+            <option value="TRY">Trichy</option>
+          </select>
         </div>
         <button onClick={() => this.onClickLogin()}>Login</button>
         <div>
