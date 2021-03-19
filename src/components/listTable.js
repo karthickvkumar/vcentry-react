86becdafd0d4062ccd760804adbac94040346c4d
kvelraj_concord <kvelraj@concord.net>
2021-03-19 09:53:59 +0530

    post methods

--- src/components/listTable.js
+++ src/components/listTable.js
@@ -1,23 +1,46 @@
 import React, { Component } from 'react';
-import axios from 'axios'; 
+import axios from 'axios';
 
 class ListTable extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      employeeList: []
+      employeeList: [],
+      login : {
+        email: '',
+        password: ''
+      }
     }
   }
 
-  listUserProfile(){
+  listUserProfile() {
     const url = "https://reqres.in/api/users?page=1";
     axios.get(url)
       .then((response) => {
-        const {data} = response;
+        const { data } = response;
         console.log(data.data)
         this.setState({
-          employeeList : data.data
+          employeeList: data.data
         })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  inputChange = (event) => {
+    this.setState({
+      login : {...this.state.login, [event.target.name] : event.target.value}
+    })
+  }
+
+  sumbitLogin(){
+    console.log(this.state.login)
+    
+    const url = "https://reqres.in/api/login";
+    axios.post(url, this.state.login)
+      .then((response) => {
+        console.log(response)
       })
       .catch((error) => {
         console.log(error)
@@ -33,7 +56,7 @@ class ListTable extends Component {
           <td>{value.last_name}</td>
           <td>{value.email}</td>
           <td>
-            <img src={value.avatar} style={{ width : '45px' }} />
+            <img src={value.avatar} style={{ width: '45px' }} />
           </td>
         </tr>
       )
@@ -41,9 +64,20 @@ class ListTable extends Component {
 
     return (
       <div>
-        <div>
-          <button onClick={() => this.listUserProfile()}>List All User Profile</button>
+        <div className="form">
+          <div className="input-wrapper">
+            <label className="label">Enter Email ID:</label>
+            <input className="input" type="email" name="email" placeholder="Please enter email id"
+              onChange={this.inputChange} />
+          </div>
+          <div className="input-wrapper">
+            <label className="label">Enter Password:</label>
+            <input className="input" type="password" name="password" placeholder="Please enter password"
+              onChange={this.inputChange} />
+          </div>
+          <button onClick={() => this.sumbitLogin()}>LOGIN</button>
         </div>
+        <button onClick={() => this.listUserProfile()}>List All User Profile</button>
         <table id="customers">
           <thead>
             <tr>
