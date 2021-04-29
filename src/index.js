108ed72558c6f9d1acaac7be26c312518720bc48
kvelraj_concord <kvelraj@concord.net>
2021-04-29 09:31:09 +0530

    redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {Provider} from 'react-redux';
+
+import configureStore from './redux/store/create-store';
 
 // import HeaderComponent from './components/header';
 // import Sidebar from './components/sidebar';
@@ -16,28 +19,32 @@ import PageNotFound from './pages/page-not-found';
 import ProtectedRoute from './components/protected-route';
 import LifeCycle from './pages/life-cycle';
 
+const configStore = configureStore();
+
 function App(){
   // const HomePage = lazy(() => import('./pages/home'));
-
+  
   return(
-   <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={LoginPage}></Route>
-        <ProtectedRoute path="/register" component={RegisterPage}></ProtectedRoute>
-        <ProtectedRoute path="/home" component={HomePage}></ProtectedRoute>
-        <ProtectedRoute path="/extra" component={LifeCycle}></ProtectedRoute>
-        {/* <Route path="/home" render={
-          () => {
-            return(
-              <Suspense fallback={<h2>Page is loading...</h2>}>
-                <HomePage></HomePage>
-              </Suspense>
-            )
-          }
-        }></Route> */}
-        <Route component={PageNotFound}></Route>
-      </Switch>
-   </BrowserRouter>
+    <Provider store={configStore}>
+    <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={LoginPage}></Route>
+          <ProtectedRoute path="/register" component={RegisterPage}></ProtectedRoute>
+          <ProtectedRoute path="/home" component={HomePage}></ProtectedRoute>
+          <ProtectedRoute path="/extra" component={LifeCycle}></ProtectedRoute>
+          {/* <Route path="/home" render={
+            () => {
+              return(
+                <Suspense fallback={<h2>Page is loading...</h2>}>
+                  <HomePage></HomePage>
+                </Suspense>
+              )
+            }
+          }></Route> */}
+          <Route component={PageNotFound}></Route>
+        </Switch>
+    </BrowserRouter>
+    </Provider>
   )
 }
 
